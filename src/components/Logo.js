47e405a657e31fb51e423e41580a711c0bb62c3c
Ali umed <aliumed844@gmail.com>
2023-07-09 22:09:30 +0300

    fix bug

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 export default function Logo() {
   return (
     <h3 className=" pt-5 text-3xl text-center font-bold text-blue-500">
-      remember your item 💲
+      remember your items 💲
     </h3>
   );
 }
