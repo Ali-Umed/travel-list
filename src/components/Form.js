4d5a72192cef400dc186c07095dedaea06940e05
Ali umed <aliumed844@gmail.com>
2023-07-12 08:31:32 +0300

    Change UX

--- src/components/Form.js
+++ src/components/Form.js
@@ -68,42 +68,6 @@ export default function Form({
     </div>
   );
 }
-//function Form2(
-//  handleSubmit,
-//  quantity,
-//  setQuantity,
-//  description,
-//  setDescription
-//) {
-//  return (
-//    <form onSubmit={handleSubmit}>
-//      <div className="flex justify-around mt-5">
-//        <h3 className="text-2xl text ">What do you want to buy ?</h3>
-//        <select
-//          className="py-3 px-4 pr-9 block  border-gray-200 rounded-full text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400"
-//          value={quantity}
-//          onChange={e => setQuantity(Number(e.target.value))}
-//        >
-//          {Array.from({ length: 20 }, (_, i) => i + 1).map(num => (
-//            <option value={num} key={num}>
-//              {num}
-//            </option>
-//          ))}
-//        </select>
-//        <input
-//          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded  py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-//          type="text"
-//          placeholder="Item"
-//          value={description}
-//          onChange={e => setDescription(e.target.value)}
-//        />
-//        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-//          Add item
-//        </button>
-//      </div>
-//    </form>
-//  );
-//}
 
 function Formm({
   quantity,
@@ -117,7 +81,7 @@ function Formm({
     <form onSubmit={handleSubmit}>
       <div
         className={` ${
-          setOpenModal ? ' relative top-48  z-10 ' : ''
+          setOpenModal ? ' relative top-38  z-10 ' : ''
         }   shadow-lg border-2 rounded-lg bg-gray-100 shadow-gray-500 `}
       >
         <h3 className="text-2xl text-center pt-3  ">
--- src/components/ListOfItem.js
+++ src/components/ListOfItem.js
@@ -30,10 +30,10 @@ export default function ListOfItem({
 
   return (
     <div className="list mt-9 bg-emerald-100/30 text-lg pb-4">
-      <h1 className=" py-10 text-center text-2xl font-bold text-blue-600">
+      <h1 className=" py-5 text-center text-3xl font-bold text-blue-600">
         list of items{' '}
       </h1>
-      <ul className="   mt-4 space-y-2  ">
+      <ul className="   mt-1 space-y-3  ">
         {sortedItems.map(item => (
           <Item
             item={item}
@@ -49,22 +49,30 @@ export default function ListOfItem({
       </ul>
 
       <div className="flex justify-evenly mt-8">
-        <select
-          className="py-3 px-4 pr-9 block   rounded-xl text-sm  border-[2px] border-violet-700 focus:border-purple-700 focus:ring-purple-500 "
-          value={sortBy}
-          onChange={e => setSortBy(e.target.value)}
-        >
-          <option value="input">Sort by input order </option>
-          <option value="description">Sort by description</option>
-          <option value="bought">Sort by buy status</option>
-        </select>
-        <button
-          className="bg-violet-700  focus:border-purple-500  rounded-xl  font-semibold text-white py-2 px-4 border  hover:border-transparent hover:bg-red-500"
-          onClick={onClearList}
-        >
-          {' '}
-          Clear list{' '}
-        </button>
+        {sortedItems.length > 0 && (
+          <select
+            className="py-3 px-4 pr-9 block   rounded-xl text-sm  border-[2px] border-violet-700 focus:border-purple-700 focus:ring-purple-500 "
+            value={sortBy}
+            onChange={e => setSortBy(e.target.value)}
+          >
+            <option value="input">Sort by input order </option>
+            <option value="description">Sort by description</option>
+            <option value="bought">Sort by buy status</option>
+          </select>
+        )}
+        {sortedItems.length > 0 && (
+          <button
+            className="bg-violet-700  focus:border-purple-500  rounded-xl  font-semibold text-white py-2 px-4 border  hover:border-transparent hover:bg-red-500"
+            onClick={onClearList}
+          >
+            Clear list
+          </button>
+        )}
+        {sortedItems.length === 0 && (
+          <h4 className=" font-bold text-xl text-center  mt-auto     py-6">
+            You dont have any item Starting adding some items to your item list{' '}
+          </h4>
+        )}
       </div>
     </div>
   );
